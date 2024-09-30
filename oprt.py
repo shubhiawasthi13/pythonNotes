3d5e87bb596279df9b29b02f3932f47e57a5a7ca
shubhi awasthi13 <awasthishubhi147@gmail.com>
2024-09-30 20:40:35 +0530

    add oprt file

---
+++ oprt.py
@@ -0,0 +1,43 @@
+#operators in python
+#Arithmetic operators
+"""
+num1 = int(input("write first num : "))
+num2 = int (input("write second num: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+"""
+#Relational/ Comparison operators
+"""
+A = 5
+B = 7
+print(A==B)
+print(A!=B)
+print(A>B)
+print(A<B)
+print(A>=B)
+print(A<=B)
+"""
+#Assignment operators
+"""
+A = 10
+#perform all operator like that
+A+=5
+print(A)
+"""
+#Logical operators
+print(not True)
+print(not False)
+print(False and True)#output: false
+print(True and False)#output: false
+print(False and False)#output: false
+print(True and True) #output: true
+
+print(False or True)#output: true
+print(True or False)#output: true
+print(False or False)#output: false
+print(True or True) #output: true
