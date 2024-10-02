25f07fec66a175a3906578b0779c8b40eb516358
shubhi awasthi13 <awasthishubhi147@gmail.com>
2024-10-02 19:50:37 +0530

    add more content

--- oprt.py
+++ oprt.py
@@ -1,4 +1,4 @@
-#operators in python
+# #operators in python
 #Arithmetic operators
 num1 = int(input("write first num : "))
 num2 = int (input("write second num: "))
@@ -38,3 +38,19 @@ print(False or True)#output: true
 print(True or False)#output: true
 print(False or False)#output: false
 print(True or True) #output: true
+
+#practice questions
+#wrirte a program to find largest of fourth number
+a = int(input("Write first number : "))
+b = int(input("Write second number : "))
+c = int(input("Write third number : "))
+d = int(input("write fourth number : "))
+
+if(a >= b and a >= c and a >= d):
+    print("number 1 is the largest number",a)
+elif(b >= c and b >= d):
+    print("number 2 is the largest number",b)
+elif(c >= d):
+    print("number 3 is the largest number",c)
+else:
+    print("number 4 is the largest number",d)       
