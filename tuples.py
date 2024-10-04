c75f821f716eb8c41570bcf6d8d9eb18c77c34ce
shubhi awasthi13 <awasthishubhi147@gmail.com>
2024-10-04 20:57:16 +0530

    add new files

---
+++ tuples.py
@@ -0,0 +1,19 @@
+#tuples in python
+tup = (45, 67, 78, 88)
+# tup[1] = 44 #immutable you can not change
+print(tup)
+
+#single value tuple
+tup = (31,)
+print(type(tup))
+
+#typle methods/functions
+tup = (2, 3, 2, 5, 2, 7)
+print(tup.index(2)) # returns index of 1st occurence
+print(tup.count(2)) #count total occ....
+
+# practice ques..
+# WAP to count the number od students with the "A" Grade in the following tuple
+tup = ("C", "D", "A", "A", "B" ,"B","A")
+print(tup.count("A"))
+
